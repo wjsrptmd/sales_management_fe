38147c8a11c029471f0b18449e15dfc7285d7ef6
wjsrptmd <factorial87@hanmail.net>
2023-01-31 00:49:07 +0900

    axios 연습 중

--- src/page/loginPage/LoginPage.js
+++ src/page/loginPage/LoginPage.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { test } from "api";
 
 export default function LoginPage() {
+  const [data, setData] = useState(null);
+  test()
+    .then(function (response) {
+      // 성공한 경우 실행
+      console.log(response.data);
+      setData(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      // 에러인 경우 실행
+      console.log(error);
+    });
   const navigate = useNavigate();
   const goMainPage = () => {
     navigate("/mainPage");
   };
+
+  const title = `LoginPage 입니다. 첫 페이지. 테스트 ${data}`;
   return (
     <div className="LoginPage">
-      LoginPage 입니다. 첫 페이지
-      <button onClick={goMainPage}>mainPage 로 이동</button>
+      {title}
+      <button onClick={goMainPage}>메인 페이지</button>
     </div>
   );
 }
