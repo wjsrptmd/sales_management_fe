6ab4bc900c8d6a8d4ef57392ff29351b3bb4952a
wjsrptmd <factorial87@hanmail.net>
2023-02-01 01:25:42 +0900

    무료 css 추가 및 로그인 페이지 임시로 만들어봄

--- src/api.js
+++ src/api.js
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+// axios 인스턴스를 만들 때 구성 기본 값 설정
+const instance = axios.create({
+  baseURL: 'http://localhost:5000'
+});
+
+// 인스턴스가 생성 된 후 기본값 변경
+// instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
 export function test() {
-  return axios.get("http://localhost:5000");
+  return instance.get('/');
 }
+
+// export function login() {
+//   return axios.get(root);
+// }
--- src/page/loginPage/LoginPage.js
+++ src/page/loginPage/LoginPage.js
@@ -1,30 +1,76 @@
 import React from "react";
+import "page/steller.css";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { test } from "api";
 
 export default function LoginPage() {
-  const [data, setData] = useState(null);
-  test()
-    .then(function (response) {
-      // 성공한 경우 실행
-      console.log(response.data);
-      setData(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      // 에러인 경우 실행
-      console.log(error);
-    });
+  // const [data, setData] = useState(null);
+  // test()
+  //   .then(function (response) {
+  //     // 성공한 경우 실행
+  //     console.log(response.data);
+  //     setData(JSON.stringify(response.data));
+  //   })
+  //   .catch(function (error) {
+  //     // 에러인 경우 실행
+  //     console.log(error);
+  //   });
+
+  const [userId, setId] = useState(undefined);
+  const [userPw, setPw] = useState(undefined);
+
   const navigate = useNavigate();
   const goMainPage = () => {
-    navigate("/mainPage");
+    const isEmpty = userId === undefined || userPw === undefined;
+
+    if (isEmpty) {
+      // TODO : userId, userPw 값이 비어 있다고 알려주자.
+    } else {
+      // let isOk = false;
+      test()
+        .then(function (response) {
+          // 성공한 경우 실행
+          console.log(response.data['message']);
+        })
+        .catch(function (error) {
+          // 에러인 경우 실행
+          console.log(error);
+        });
+      navigate("/mainPage");
+    }
   };
 
-  const title = `LoginPage 입니다. 첫 페이지. 테스트 ${data}`;
   return (
     <div className="LoginPage">
-      {title}
-      <button onClick={goMainPage}>메인 페이지</button>
+      <div className="login">
+        <div className="form-group">
+          <h3>Login</h3>
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="ID 를 입력해 주세요."
+            value={userId || undefined}
+            onChange={(e) => setId(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="비밀번호를 입력해 주세요."
+            value={userPw || undefined}
+            onChange={(e) => setPw(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <button className="btn btn-primary" onClick={goMainPage}>
+            로그인
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
